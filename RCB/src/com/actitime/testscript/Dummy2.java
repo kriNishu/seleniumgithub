fb1bbb5253469a312cc2835846e5700b0f8d27aa
VINAY C N <VINAY C N@LAPTOP-VVQO476E>
2024-03-01 18:02:32 +0530

    VINAY

---
+++ RCB/src/com/actitime/testscript/Dummy2.java
@@ -0,0 +1,13 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Dummy2 {
+	 @Test
+	 public void dummyModule()
+	 {
+		 Reporter.log("pushing test case", true);
+		 
+	 }
+}
