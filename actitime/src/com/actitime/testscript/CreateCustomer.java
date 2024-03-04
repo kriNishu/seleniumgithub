4b305d76a5509a7e19aebe389d52e8578b147310
nishu <nishu@NISHU>
2024-03-04 14:00:45 +0530

    nishu

---
+++ actitime/src/com/actitime/testscript/CreateCustomer.java
@@ -0,0 +1,17 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.actitime.generic.Baseclass;
+
+public class CreateCustomer extends Baseclass {
+	
+		
+		@Test
+		public void createCustomer()
+		{
+			Reporter.log("Create Customer",true);
+		}
+
+}
